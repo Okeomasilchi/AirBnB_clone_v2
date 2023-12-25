a91f16c909cec25d3185c05553461c6de2eee621
Okeomasilachi <okeomasilachi@gmail.com>
2023-12-25 16:38:03 +0100

    Write a script that starts a Flask web application:
    Your web application must be listening on 0.0.0.0, port 5000
    Routes:
    /: display “Hello HBNB!”
    /hbnb: display “HBNB”
    /c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )
    You must use the option strict_slashes=False in your route definition

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,7 +7,7 @@ Routes:
     /: display “Hello HBNB!”
     /hbnb: display “HBNB”
     /c/<text>: display “C ” followed by the value of
-        the text variable (replace underscore 
+        the text variable (replace underscore
         _ symbols with a space )
 """
 
