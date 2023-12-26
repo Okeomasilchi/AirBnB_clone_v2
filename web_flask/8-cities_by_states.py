2bf73d50e149fb6f098d9560b00cf17367cdd1ea
Okeomasilachi <okeomasilachi@gmail.com>
2023-12-27 00:39:12 +0100

    Write a script that starts a Flask web application:
    Your web application must be listening on 0.0.0.0, port 5000
    You must use storage for fetching data from the storage engine (FileStorage or DBStorage) => from models import storage and storage.all(...)
    To load all cities of a State:
    If your storage engine is DBStorage, you must use cities relationship
    Otherwise, use the public getter method cities
    After each request you must remove the current SQLAlchemy Session:
    Declare a method to handle @app.teardown_appcontext
    Call in this method storage.close()
    Routes:
    /cities_by_states: display a HTML page: (inside the tag BODY)
    H1 tag: “States”
    UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip
    LI tag: description of one State: <state.id>: <B><state.name></B> + UL tag: with the list of City objects linked to the State sorted by name (A->Z)
    LI tag: description of one City: <city.id>: <B><city.name></B>
    Import this 7-dump to have some data
    You must use the option strict_slashes=False in your route definition

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,6 +8,7 @@ Starts a Flask web application
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 
 app = Flask(__name__)
 
@@ -18,18 +19,24 @@ def teardown(exception):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Display a HTML page with a list of all State objects"""
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a HTML page with a list of all State & city objects"""
     states = storage.all(State)
     ret = []
     sorted_states = dict(sorted(states.items(), key=lambda x: x[1]['name']))
     for state in sorted_states.items():
         state = dict(state[1])
         ret.append(state)
-    return render_template('7-states_list.html', data=ret)
+    cities = storage.all(City)
+    ret_cities = []
+    sorted_cities = dict(sorted(cities.items(), key=lambda x: x[1]['name']))
+    for city in sorted_cities.items():
+        city = dict(city[1])
+        ret_cities.append(city)
+    return render_template('8-cities_by_states.html', data=ret, cities=ret_cities)
 
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
