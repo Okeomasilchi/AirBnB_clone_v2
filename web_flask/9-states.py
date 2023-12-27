7ec09cdb265acad8674f8813dde13d562292f296
Okeomasilachi <okeomasilachi@gmail.com>
2023-12-27 11:28:34 +0100

    Write a script that starts a Flask web application:
    Your web application must be listening on 0.0.0.0, port 5000
    You must use storage for fetching data from the storage engine (FileStorage or DBStorage) => from models import storage and storage.all(...)
    To load all cities of a State:
    If your storage engine is DBStorage, you must use cities relationship
    Otherwise, use the public getter method cities
    After each request you must remove the current SQLAlchemy Session:
    Declare a method to handle @app.teardown_appcontext
    Call in this method storage.close()
    Routes:
    /states: display a HTML page: (inside the tag BODY)
    H1 tag: “States”
    UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip
    LI tag: description of one State: <state.id>: <B><state.name></B>
    /states/<id>: display a HTML page: (inside the tag BODY)
    If a State object is found with this id:
    H1 tag: “State: ”
    H3 tag: “Cities:”
    UL tag: with the list of City objects linked to the State sorted by name (A->Z)
    LI tag: description of one City: <city.id>: <B><city.name></B>
    Otherwise:
    H1 tag: “Not found!”
    You must use the option strict_slashes=False in your route definition
    Import this 7-dump to have some data

---
+++ web_flask/9-states.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+
+"""
+Starts a Flask web application
+"""
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+def sort_dict(Class, by="name"):
+    """
+    sorts a dictionary of classes based on a
+    specified attribute and returns the sorted dictionary.
+
+    Class: expected to be a dictionary where the
+        keys are the names of the objects and the
+        values are dictionaries containing attributes
+        of the objects
+    by: used to specify the key by which the dictionary
+        should be sorted. By default, it is set to "name",
+        which means the dictionary will be sorted by the
+        values of the "name" key in ascending order.
+        However, you can change the value of "by",
+        defaults to name (optional)
+    return: a sorted dictionary.
+    """
+
+    return dict(sorted(Class.items(), key=lambda x: x[1][by]))
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states_list():
+    """Display a HTML page with a list of all State objects"""
+    states = storage.all(State)
+    ret = []
+    sorted_states = sort_dict(states)
+
+    for state in sorted_states.items():
+        state = dict(state[1])
+        ret.append(state)
+
+    return render_template('9-states.html', data=ret)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def cities_by_states(id):
+    """Display a HTML page with a list of all State & city objects"""
+    states = storage.all(State)
+    cities = storage.all(City)
+    ret_cities = []
+    sorted_cities = sort_dict(cities)
+
+    if f"State.{id}" in states:
+        states = states[f"State.{id}"]
+    else:
+        states = None
+
+    if states:
+        for city in sorted_cities.items():
+            city = dict(city[1])
+            if city["state_id"] == states["id"]:
+                ret_cities.append({"id": city['id'], "name": city['name']})
+        states["cities"] = ret_cities
+
+    return render_template('9-states.html', state_city=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
