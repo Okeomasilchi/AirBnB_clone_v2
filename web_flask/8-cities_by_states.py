7f8ea8a9ee7ce4f4c3357186c0c7d656ed85f6ce
Okeomasilachi <okeomasilachi@gmail.com>
2023-12-27 08:47:43 +0100

    Write a script that starts a Flask web application:
    Your web application must be listening on 0.0.0.0, port 5000
    You must use storage for fetching data from the storage engine (FileStorage or DBStorage) => from models import storage and storage.all(...)
    To load all cities of a State:
    If your storage engine is DBStorage, you must use cities relationship
    Otherwise, use the public getter method cities
    After each request you must remove the current SQLAlchemy Session:
    Declare a method to handle @app.teardown_appcontext
    Call in this method storage.close()
    Routes:
    /cities_by_states: display a HTML page: (inside the tag BODY)
    H1 tag: “States”
    UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip
    LI tag: description of one State: <state.id>: <B><state.name></B> + UL tag: with the list of City objects linked to the State sorted by name (A->Z)
    LI tag: description of one City: <city.id>: <B><city.name></B>
    Import this 7-dump to have some data
    You must use the option strict_slashes=False in your route definition

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -19,23 +19,49 @@ def teardown(exception):
     storage.close()
 
 
+def sort_dict(Class, by="name"):
+    """
+    sorts a dictionary of classes based on a
+    specified attribute and returns the sorted dictionary.
+
+    Class: expected to be a dictionary where the
+        keys are the names of the objects and the
+        values are dictionaries containing attributes
+        of the objects
+    by: used to specify the key by which the dictionary
+        should be sorted. By default, it is set to "name",
+        which means the dictionary will be sorted by the
+        values of the "name" key in ascending order.
+        However, you can change the value of "by",
+        defaults to name (optional)
+    return: a sorted dictionary.
+    """
+    return dict(sorted(Class.items(), key=lambda x: x[1][by]))
+
+
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """Display a HTML page with a list of all State & city objects"""
     states = storage.all(State)
+    cities = storage.all(City)
     ret = []
-    sorted_states = dict(sorted(states.items(), key=lambda x: x[1]['name']))
+    ret_cities = []
+    sorted_states = sort_dict(states)
+    sorted_cities = sort_dict(cities)
+
     for state in sorted_states.items():
         state = dict(state[1])
         ret.append(state)
-    cities = storage.all(City)
-    ret_cities = []
-    sorted_cities = dict(sorted(cities.items(), key=lambda x: x[1]['name']))
-    for city in sorted_cities.items():
-        city = dict(city[1])
-        ret_cities.append(city)
-    return render_template('8-cities_by_states.html', data=ret, cities=ret_cities)
 
+    for i in ret:
+        for city in sorted_cities.items():
+            city = dict(city[1])
+            if city["state_id"] == i["id"]:
+                ret_cities.append({"id": city['id'], "name": city['name']})
+        i["cities"] = ret_cities
+        ret_cities = []
+
+    return render_template('8-cities_by_states.html', data=ret)
 
 
 if __name__ == '__main__':
